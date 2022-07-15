967624d80fc8303db61759d27deef6911b3d3410
kevinluo-hub <rongqiangluo@umass.edu>
2022-07-14 23:59:52 -0400

    styled nav button, needs to add scrolling ability

--- src/App.js
+++ src/App.js
@@ -3,14 +3,17 @@ import Header from './components/Header'
 import Intro from './components/Intro'
 import Skills from './components/Skills'
 import Title from './components/Title'
+import About from './components/About'
 
 function App() {
   return (
     <div>
       <Header title="鏘." items={["Skills", "About", "Gallery", "Contact"]}/>
       <Intro text={'Hello!\nI\'m Kevin Luo,\na developer and bodybuilder\nbased in Boston, MA'}/>
-      <Title text="Skills" />
+      <Title className="Skills" text="Skills" />
       <Skills />
+      <Title ClassName="About" text="About" />
+      <About />
     </div>
   );
 }
